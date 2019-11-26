3304bb270fd1cd5d377f0fc03738bb3e70ef1d59
Marshall Sykes <rmsykes91@gmail.com>
2019-11-26 11:13:20 -0500

    working on creating new action withing one dog page. able to update state for my newAction walk input for my form. will continue building out my form

--- client/src/components/OneDog.jsx
+++ client/src/components/OneDog.jsx
@@ -12,15 +12,50 @@ export default class OneDog extends Component {
             breed: '',
             photo_url: '',
             owner: '',
+            // actions: {
+            //     walk: '',
+            //     eat: '',
+            //     poop:'',
+            //     pee: '',
+            //     medicine: ''
+            // },
+        },
+        actionData: [],
+        newAction: {
+            walk: '',
+            eat: '',
+            poop: '',
+            pee: '',
+            medicine: '',
+            dog: '',
         }
     }
 
 
-    componentDidMount() {
+    componentDidMount = () => {
         axios.get(`/api/v1/dog/${this.props.match.params.dogId}`)
         .then((res) => {
             this.setState({ dog: res.data })
         })
+  
+    }
+
+
+    createNewAction = (evt) => {
+        evt.preventDefault()
+        const newAction = this.state.newAction
+
+        axios.post(`/api/v1/action`, newAction)
+        .then((res) => {
+            this.componentDidMount()
+        })
+    }
+
+
+    handleInputChange = (evt) => {
+        const copiedNewAction = {...this.state.newAction}
+        copiedNewAction[evt.target.name] = evt.target.value
+        this.setState({ newAction: copiedNewAction })
     }
 
 
@@ -38,7 +73,22 @@ export default class OneDog extends Component {
                 <img src={this.state.dog.photo_url} alt="dog photo"/>
 
 
-                
+                <div className='createForm'>
+                    <form >
+                        <h2>Record Dog Action</h2>
+                        <input 
+                            type="string"
+                            name="walk"
+                            placeholder="Record Walk"
+                            onChange={this.handleInputChange}
+                            value={this.state.newAction.walk}
+                        />
+
+                        <input type='submit' value='Record New Action'/>
+                    </form>
+
+                </div>
+
 
             </div>
         )
